1d085ee9e214486ec2bec57277d41f98d94bed9a
armaBurton <armanarma@armaburton.com>
2021-10-06 23:06:53 -0700

    added overlay and animation to overlay

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const daddy = document.getElementById('daddy');
 const tumor = document.getElementById('tumor');
 const cookie = document.getElementById('cookie');
 
+const overlay01 = document.getElementById('overlay01');
+const overlay02 = document.getElementById('overlay02');
+const overlay03 = document.getElementById('overlay03');
+const overlay04 = document.getElementById('overlay04');
+const overlay05 = document.getElementById('overlay05');
+const overlay06 = document.getElementById('overlay06');
+const overlay07 = document.getElementById('overlay07');
+const overlay08 = document.getElementById('overlay08');
+const overlay09 = document.getElementById('overlay09');
+
 const freezeAudio = new Audio('assets/01-Freeze.mp3');
 const terminatorAudio = new Audio('assets/02-terminator.mp3');
 const behindAudio = new Audio('assets/03-behind.mp3');
@@ -28,94 +38,166 @@ const cookieAudio = new Audio('assets/09-cookie.mp3');
 freeze.addEventListener('click', () => {
     freezeAudio.play();
     freeze.classList.add('image-animate');
-    setTimeout(() => {freeze.classList.remove('image-animate');}, 4001);
+    overlay01.classList.add('image-animate');
+    setTimeout(() => {
+        freeze.classList.remove('image-animate');
+        overlay01.classList.remove('image-animate');
+    }, 4001);
 });
 
 terminator.addEventListener('click', () => {
     terminatorAudio.play();
     terminator.classList.add('image-animate');
-    setTimeout(() => {terminator.classList.remove('image-animate');}, 4001);
+    overlay02.classList.add('image-animate');
+    setTimeout(() => {
+        terminator.classList.remove('image-animate');
+        overlay02.classList.remove('image-animate');
+    }, 4001);
 });
 
 behind.addEventListener('click', () => {
     behindAudio.play();
     behind.classList.add('image-animate');
-    setTimeout(() => {behind.classList.remove('image-animate');}, 4001);
+    overlay03.classList.add('image-animate');
+    setTimeout(() => {
+        behind.classList.remove('image-animate');
+        overlay03.classList.remove('image-animate');
+    }, 4001);
 });
 
 choppa.addEventListener('click', () => {
     choppaAudio.play();
     choppa.classList.add('image-animate');
-    setTimeout(() => {choppa.classList.remove('image-animate');}, 4001);
+    overlay04.classList.add('image-animate');
+    setTimeout(() => {
+        choppa.classList.remove('image-animate');
+        overlay04.classList.remove('image-animate');
+    }, 4001);
 });
 
 questions.addEventListener('click', () => {
     questionsAudio.play();
     questions.classList.add('image-animate');
-    setTimeout(() => {questions.classList.remove('image-animate');}, 4001);
+    overlay05.classList.add('image-animate');
+    setTimeout(() => {
+        questions.classList.remove('image-animate');
+        overlay05.classList.remove('image-animate');
+    }, 4001);
 });
 
 bathroom.addEventListener('click', () => {
     bathroomAudio.play();
     bathroom.classList.add('image-animate');
-    setTimeout(() => {bathroom.classList.remove('image-animate');}, 4001);
+    overlay06.classList.add('image-animate');
+    setTimeout(() => {
+        bathroom.classList.remove('image-animate');
+        overlay06.classList.remove('image-animate');
+    }, 4001);
 });
 
 daddy.addEventListener('click', () => {
     daddyAudio.play();
     daddy.classList.add('image-animate');
-    setTimeout(() => {daddy.classList.remove('image-animate');}, 4001);
+    overlay07.classList.add('image-animate');
+    setTimeout(() => {
+        daddy.classList.remove('image-animate');
+        overlay07.classList.remove('image-animate');
+    }, 4001);
 });
 
 tumor.addEventListener('click', () => {
     tumorAudio.play();
     tumor.classList.add('image-animate');
-    setTimeout(() => {tumor.classList.remove('image-animate');}, 4001);
+    overlay08.classList.add('image-animate');
+    setTimeout(() => {
+        tumor.classList.remove('image-animate');
+        overlay08.classList.remove('image-animate');
+    }, 4001);
 });
 
 cookie.addEventListener('click', () => {
     cookieAudio.play();
     cookie.classList.add('image-animate');
-    setTimeout(() => {cookie.classList.remove('image-animate');}, 4001);
+    overlay09.classList.add('image-animate');
+    setTimeout(() => {
+        cookie.classList.remove('image-animate');
+        overlay09.classList.remove('image-animate');
+    }, 4001);
 });
 
 document.addEventListener('keydown', (e) => {
     if (e.key == 1){
         freezeAudio.play();
         freeze.classList.add('image-animate');
-        setTimeout(() => {freeze.classList.remove('image-animate');}, 4001);
+        overlay01.classList.add('image-animate');
+        setTimeout(() => {
+            freeze.classList.remove('image-animate');
+            overlay01.classList.remove('image-animate');
+        }, 4001);
     } else if (e.key == 2){
         terminatorAudio.play();
         terminator.classList.add('image-animate');
-        setTimeout(() => {terminator.classList.remove('image-animate');}, 4001);
+        overlay02.classList.add('image-animate');
+        setTimeout(() => {
+            terminator.classList.remove('image-animate');
+            overlay02.classList.remove('image-animate');
+        }, 4001);
     } else if (e.key == 3){
         behindAudio.play();
         behind.classList.add('image-animate');
-        setTimeout(() => {behind.classList.remove('image-animate');}, 4001);
+        overlay03.classList.add('image-animate');
+        setTimeout(() => {
+            behind.classList.remove('image-animate');
+            overlay03.classList.remove('image-animate');
+        }, 4001);
     } else if (e.key == 4){
         choppaAudio.play();
         choppa.classList.add('image-animate');
-        setTimeout(() => {choppa.classList.remove('image-animate');}, 4001);
+        overlay04.classList.add('image-animate');
+        setTimeout(() => {
+            choppa.classList.remove('image-animate');
+            overlay04.classList.remove('image-animate');
+        }, 4001);
     } else if (e.key == 5){
         questionsAudio.play();
         questions.classList.add('image-animate');
-        setTimeout(() => {questions.classList.remove('image-animate');}, 4001);
+        overlay05.classList.add('image-animate');
+        setTimeout(() => {
+            questions.classList.remove('image-animate');
+            overlay05.classList.remove('image-animate');
+        }, 4001);
     } else if (e.key == 6){
         bathroomAudio.play();
         bathroom.classList.add('image-animate');
-        setTimeout(() => {bathroom.classList.remove('image-animate');}, 4001);
+        overlay06.classList.add('image-animate');
+        setTimeout(() => {
+            bathroom.classList.remove('image-animate');
+            overlay06.classList.remove('image-animate');
+        }, 4001);
     } else if (e.key == 7){
         daddyAudio.play();
         daddy.classList.add('image-animate');
-        setTimeout(() => {daddy.classList.remove('image-animate');}, 4001);
+        overlay07.classList.add('image-animate');
+        setTimeout(() => {
+            daddy.classList.remove('image-animate');
+            overlay07.classList.remove('image-animate');
+        }, 4001);
     } else if (e.key == 8){
         tumorAudio.play();
         tumor.classList.add('image-animate');
-        setTimeout(() => {tumor.classList.remove('image-animate');}, 4001);
+        overlay08.classList.add('image-animate');
+        setTimeout(() => {
+            tumor.classList.remove('image-animate');
+            overlay08.classList.remove('image-animate');
+        }, 4001);
     } else if (e.key == 9){
         cookieAudio.play();
         cookie.classList.add('image-animate');
-        setTimeout(() => {cookie.classList.remove('image-animate');}, 4001);
+        overlay09.classList.add('image-animate');
+        setTimeout(() => {
+            cookie.classList.remove('image-animate');
+            overlay09.classList.remove('image-animate');
+        }, 4001);
     }
 
     
